465fbfa844f25d9ad4ce37897e0ae420add539f4
Gbenga Ajiboye <gbenga.aj66@gmail.com>
2024-08-20 13:41:28 +0100

    add: constants file

---
+++ src/constants.ts
@@ -0,0 +1,15 @@
+import 'dotenv/config';
+// dotenv.config();
+
+export const username = process.env.MANIFOLD_USERNAME;
+export const key = process.env.MANIFOLD_API_KEY;
+export const slug = process.env.MANIFOLD_MARKET_SLUG;
+
+
+export const randomTradeSlugs = ["will-tim-walz-have-positive-favorab",
+    "will-there-be-an-mpox-pandemic-by-2","will-taylor-swift-attend-the-dnc", "will-a-robot-be-able-to-reliably-ti", "will-samsung-galaxy-s25-ultra-have"]
+
+export const traders = [{traderApiKey: "", traderUsername: "GbengaAjiboye"}, {traderApiKey: "", traderUsername: "GbengaAjiboyehl1g"}]
+
+    
+    
